ef7e5b38202f0e77cbb99c6c2136cdf8e657eb0d
Karthik <karthik3497@gmail.com>
2024-04-03 08:59:18 +0530

    85.9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,12 +16,12 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyDQ4QsQ5w2xJL9clUPsvhWRbYT1fFyCuec",
-  authDomain: "performolabs-9643f.firebaseapp.com",
-  projectId: "performolabs-9643f",
-  storageBucket: "performolabs-9643f.appspot.com",
-  messagingSenderId: "517522912277",
-  appId: "1:517522912277:web:b1a8253009e63fe1aeca1c",
-  measurementId: "G-Y12640164E"
+    authDomain: "performolabs-9643f.firebaseapp.com",
+    projectId: "performolabs-9643f",
+    storageBucket: "performolabs-9643f.appspot.com",
+    messagingSenderId: "517522912277",
+    appId: "1:517522912277:web:b1a8253009e63fe1aeca1c",
+    measurementId: "G-Y12640164E"
 };
 
 // Initialize Firebase
